cb0f1a4480f364525451fd0292fc746d20dfe137
Simon Constans <simonc@theodo.fr>
2013-01-04 12:26:50 +0100

    change redirection

--- src/chlorius/ChloriusBundle/Controller/DefaultController.php
+++ src/chlorius/ChloriusBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace chlorius\ChloriusBundle\Controller;
 
+use JMS\SecurityExtraBundle\Annotation\Secure;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -14,9 +15,11 @@ class DefaultController extends Controller
 {
     /**
      * @Route("", name="homepage")
+     * @Secure(roles="ROLE_USER")
      */
-    public function indexAction($path = "")
+    public function indexAction()
     {
+
         return $this->redirect($this->generateUrl('chlorius_gallery_index'));
     }
 }
